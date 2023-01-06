d91d099701b2615211a6e384b1b78bcfe32e314c
DanDecoito <116022076+DanDecoito@users.noreply.github.com>
2023-01-06 13:41:26 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+
+let btnOne = document.getElementById("btnOne");
+let btnTwo = document.getElementById("btnTwo");
+let btnThree = document.getElementById("btnThree");
+let btnFour = document.getElementById("btnFour");
+let btnFive = document.getElementById("btnFive");
+let btnSix = document.getElementById("btnSix");
+let btnSeven = document.getElementById("btnSeven");
+let btnEight = document.getElementById("btnEight");
+let btnNine = document.getElementById("btnNine");
+let btnTen = document.getElementById("btnTen");
+
+let changeMeOne = document.getElementById("changeMeOne");
+let changeMeTwo = document.getElementById("changeMeTwo");
+let changeMeThree = document.getElementById("changeMeThree");
+let changeMeFour = document.getElementById("changeMeFour");
+let changeMeFive = document.getElementById("changeMeFive");
+let changeMeSix = document.getElementById("changeMeSix");
+let changeMeSeven = document.getElementById("changeMeSeven");
+let changeMeEight = document.getElementById("changeMeEight");
+let changeMeNine = document.getElementById("changeMeNine");
+let changeMeTen = document.getElementById("changeMeTen");
+
+
+
+
+
+
+
+
+btnOne.addEventListener("click", function(){
+    changeMeOne.innerText = "Never gonna";
+})
+btnTwo.addEventListener("click", function(){
+    changeMeTwo.innerText = "Give you up";
+})
+btnThree.addEventListener("click", function(){
+    changeMeThree.innerText = "Never gonna";
+})
+btnFour.addEventListener("click", function(){
+    changeMeFour.innerText = "Let you";
+})
+btnFive.addEventListener("click", function(){
+    changeMeFive.innerText = "Down";
+})
+btnSix.addEventListener("click", function(){
+    changeMeSix.innerText = "Never gonna";
+})
+btnSeven.addEventListener("click", function(){
+    changeMeSeven.innerText = "Turn ";
+})
+btnEight.addEventListener("click", function(){
+    changeMeEight.innerText = "around";
+})
+btnNine.addEventListener("click", function(){
+    changeMeNine.innerText = "and";
+})
+btnTen.addEventListener("click", function(){
+    changeMeTen.innerText = "Desert you";
+})
